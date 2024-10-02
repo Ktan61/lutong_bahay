93b626e4aff33974d826a4cad85f34ef013aa638
Kathryn Tan <144634477+Ktan61@users.noreply.github.com>
2024-10-02 00:17:28 -0700

    Created Forum, Recipes, and Shopping list page; installed react-router-dom and Material UI

---
+++ src/pages/ShoppingList.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header/index.jsx'
+
+export default function ShoppingList() {
+    return(
+        <div>
+            <Header />
+            This is the SHOPPING LIST
+        </div>
+    )
+}
+
