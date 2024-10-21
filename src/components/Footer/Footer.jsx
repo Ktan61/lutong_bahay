b2ecac5468d7151d46d29ab280f709a78c61f320
1medi <144403286+1medi@users.noreply.github.com>
2024-10-21 10:54:08 -0700

    wokr PLEASE

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './Footer.css';
 
 const Footer = () => {
@@ -14,10 +14,10 @@ const Footer = () => {
           <li><a href="#">Contact</a></li>
         </ul>
         <div className="footer-social">
-          <a href="https://facebook.com"><FontAwesomeIcon icon={faFacebook} /></a>
+          {/* <a href="https://facebook.com"><FontAwesomeIcon icon={faFacebook} /></a>
           <a href="https://twitter.com"><FontAwesomeIcon icon={faTwitter} /></a>
           <a href="https://instagram.com"><FontAwesomeIcon icon={faInstagram} /></a>
-          <a href="mailto:info@example.com"><FontAwesomeIcon icon={faEnvelope} /></a>
+          <a href="mailto:info@example.com"><FontAwesomeIcon icon={faEnvelope} /></a> */}
         </div>
       </div>
       <div className="footer-bottom">
