30d95eb9fa8cffe9a33e0694651d45f89b5bc897
Kathryn Tan <144634477+Ktan61@users.noreply.github.com>
2024-11-17 03:39:33 -0800

    Updated functionality for filters and search bar in Shopping List

--- src/components/ShoppingListCard/index.jsx
+++ src/components/ShoppingListCard/index.jsx
@@ -7,8 +7,7 @@ import { FormGroup, FormControlLabel, Checkbox } from '@mui/material';
 import { useState } from 'react';
 
 export default function ShoppingListCard(props) {
-
-    const { myShoppingList } = props;
+    const { myShoppingList, handleDelete } = props;
 
     const [checkedItems, setCheckedItems] = useState(
         myShoppingList.ingredients.map(() => false) 
@@ -22,46 +21,43 @@ export default function ShoppingListCard(props) {
         setCheckedItems(updatedCheckedItems);
     };
 
-
-    return(
-        <>
-            <div className={styles.container}>
-                <div
-                    className={styles.imageContainer}
-                    onMouseEnter={() => setIsHovered(true)} // Show overlay on hover
-                    onMouseLeave={() => setIsHovered(false)} // Hide overlay when not hovering
-                >
-                    <img 
-                        alt={myShoppingList.altText} 
-                        src={myShoppingList.image} 
-                        width={468} 
-                        height={328} 
-                        className={styles.image}
-                    />
-                    {isHovered && (
-                        <div className={styles.imageOverlay}>
-                            <div className={styles.imageOverlayContent}>
-                                <CancelIcon fontSize='large'/>
-                            </div>
+    return (
+        <div className={styles.container}>
+            <div
+                className={styles.imageContainer}
+                onMouseEnter={() => setIsHovered(true)} 
+                onMouseLeave={() => setIsHovered(false)} 
+            >
+                <img 
+                    alt={myShoppingList.altText} 
+                    src={myShoppingList.image} 
+                    width={468} 
+                    height={328} 
+                    className={styles.image}
+                />
+                {isHovered && (
+                    <div className={styles.imageOverlay}>
+                        <div className={styles.imageOverlayContent}>
+                            <CancelIcon fontSize='large' onClick={(e) => { e.stopPropagation(); handleDelete(); }} className={styles.deleteButton}/>
                         </div>
-                    )}
-                </div>
-                <div className={styles.cardHeading}>
-                    <h3 className={styles.headingName}>{myShoppingList.name}</h3>
-                    <h5 className={styles.headingQty}>Qty</h5>
-                </div>
-                <ul className={styles.ingredientsContainer}>
-                    {myShoppingList.ingredients.map((ingredient, index) => (
-                        <li key={index} className={`${styles.ingredientItem} ${checkedItems[index] ? styles.strikethrough : ""}`} >
-                            {/*<div className={styles.ingredientsLeft}>
-                                <input type='checkbox' className={styles.checkbox} />
-                                <span className={styles.ingredientName}>{ingredient[0]}</span> 
-                                {ingredient[2] === "specialty" ? <SearchIcon className={styles.specialtyIcon} fontSize="small"/> : null}
-                            </div>*/}
-                            <div className={styles.ingredientsLeft}>
-                                <FormGroup>
+                    </div>
+                )}
+            </div>
+            <div className={styles.cardHeading}>
+                <h3 className={styles.headingName}>{myShoppingList.name}</h3>
+                <h5 className={styles.headingQty}>Qty</h5>
+            </div>
+            <h6 className={styles.totalCookTime}>
+                Total Cook Time: 
+                {myShoppingList.totalCookTime[0] > 0 ? ` ${myShoppingList.totalCookTime[0]} hr` : ''} 
+                {myShoppingList.totalCookTime[1] > 0 || myShoppingList.totalCookTime[0] > 0 ? ` ${myShoppingList.totalCookTime[1]} min` : ''}
+            </h6>
+            <ul className={styles.ingredientsContainer}>
+                {myShoppingList.ingredients.map((ingredient, index) => (
+                    <li key={index} className={`${styles.ingredientItem} ${checkedItems[index] ? styles.strikethrough : ""}`}>
+                        <div className={styles.ingredientsLeft}>
+                            <FormGroup>
                                 <FormControlLabel
-                                    key={index}
                                     control={
                                         <Checkbox
                                             checked={checkedItems[index]} 
@@ -70,34 +66,29 @@ export default function ShoppingListCard(props) {
                                             sx={{
                                                 color: 'var(--yellow)',
                                                 '&.Mui-checked': {
-                                                    color: 'var(--blue)', // Custom color for the checked state
+                                                    color: 'var(--blue)', 
                                                 }
                                             }}
                                         />
                                     }
-                                    label={
-                                        <span className={styles.ingredientsLeft}
-                                        >
-                                            {ingredient[0]} 
-                                        </span>
-                                    }
+                                    label={<span>{ingredient[0]}</span>}
                                 />
-                                </FormGroup>
-                                {ingredient[2] === "specialty" ? 
-                                    <Tooltip title={
-                                        <Typography sx={{ p: 1, fontSize: "16px"}}>
-                                            Find this specialty ingredient at:<br />
-                                            <a href="https://www.instagram.com/explore/locations/432744080540085/tama-supermarket-limit/" target="_blank" rel="noopener noreferrer" style={{ color: 'var(--yellow)', textDecoration: 'none' }}>Tama Supermarket →</a> <br />
-                                            <a href="https://g.co/kgs/r5sXfYk" target="_blank" rel="noopener noreferrer" style={{ color: 'var(--yellow)', textDecoration: 'none' }}>Kay Market →</a>
-                                        </Typography>}>
-                                        <HelpSharpIcon className={styles.specialtyIcon} fontSize="small"/>
-                                    </Tooltip> : null }
-                            </div>
-                            <div className={styles.ingredientAmount}>{ingredient[1]}</div> 
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </>
+                            </FormGroup>
+                            {ingredient[2] === "specialty" ? 
+                                <Tooltip title={
+                                    <Typography sx={{ p: 1, fontSize: "16px"}}>
+                                        Find this specialty ingredient at:<br />
+                                        <a href="https://www.instagram.com/explore/locations/432744080540085/tama-supermarket-limit/" target="_blank" rel="noopener noreferrer" style={{ color: 'var(--yellow)', textDecoration: 'none' }}>Tama Supermarket →</a> <br />
+                                        <a href="https://g.co/kgs/r5sXfYk" target="_blank" rel="noopener noreferrer" style={{ color: 'var(--yellow)', textDecoration: 'none' }}>Kay Market →</a>
+                                    </Typography>
+                                }>
+                                    <HelpSharpIcon className={styles.specialtyIcon} fontSize="small"/>
+                                </Tooltip> : null }
+                        </div>
+                        <div className={styles.ingredientAmount}>{ingredient[1]}</div>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-};
+}
--- src/pages/ShoppingList.jsx
+++ src/pages/ShoppingList.jsx
@@ -1,42 +1,119 @@
-import Header from '../components/Header/index.jsx'
-import ShoppingListCard from '../components/ShoppingListCard/index.jsx'
+import { useState, useEffect } from 'react';
+import Header from '../components/Header/index.jsx';
+import ShoppingListCard from '../components/ShoppingListCard/index.jsx';
 import styles from '../styles/ShoppingList.module.css'; 
 import SearchIcon from '@mui/icons-material/Search';
-import recipe_list from '../data/list_recipes.json'
-import '@fontsource/berkshire-swash';
-import '@fontsource/poppins';
-import '@fontsource/koulen';
+import recipe_list from '../data/list_recipes.json';
 
-export default function ShoppingList() {
+const ShoppingList = () => {
+    const [allRecipes, setAllRecipes] = useState(recipe_list.recipes);
+    const [filteredRecipes, setFilteredRecipes] = useState(recipe_list.recipes);
+    const [categoryFilter, setCategoryFilter] = useState('allItems');
+    const [cookTimeFilter, setCookTimeFilter] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
 
-    const recipes = recipe_list.recipes;
+    useEffect(() => {
+        filterRecipes();
+    }, [categoryFilter, cookTimeFilter, searchQuery, allRecipes]);
 
-    return(
-        <>
-            <Header />
-            <div className={styles.container}>
+    const filterRecipes = () => {
+        let filteredData = allRecipes;
+
+        if (categoryFilter !== 'allItems') {
+            filteredData = filteredData.filter(recipe => recipe.category === categoryFilter);
+        }
+
+        if (cookTimeFilter) {
+            filteredData = filteredData.filter(recipe => {
+                const [hours, minutes] = recipe.totalCookTime;
+                const totalMinutes = (hours * 60) + minutes;
+                switch (cookTimeFilter) {
+                    case 'under30':
+                        return totalMinutes < 30;
+                    case '30to60':
+                        return totalMinutes >= 30 && totalMinutes <= 60;
+                    case 'over1hour':
+                        return totalMinutes > 60;
+                    case 'over2hours':
+                        return totalMinutes > 120;
+                    default:
+                        return true;
+                }
+            });
+        }
+
+        if (searchQuery) {
+            filteredData = filteredData.filter(recipe => 
+                recipe.name.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+        }
+
+        setFilteredRecipes(filteredData);
+    };
+
+    const handleDelete = (id) => {
+        const updatedRecipes = allRecipes.filter(recipe => recipe.id !== id);
+        setAllRecipes(updatedRecipes);
+    };
+
+    return (
+       <>
+         <Header />
+         <div className={styles.container}>
+            <section className={styles.filterHeader}>
                 <h1 className={styles.title}>Shopping List</h1>
-                <section className={styles.sortButtons}>
-                    <div className={styles.buttonContainer}>
-                        <button className={styles.button}>All Items</button>
-                        <button className={styles.button} style={{backgroundColor: 'var(--yellow)'}}>By Recipe</button>
-                        <button className={styles.button}>By Store</button>
-                        <button className={styles.button}>Total Cook Time</button>
-                    </div>
+                <div className={styles.filterHeaderRight}>
+                    <select
+                        name="categoryFilter"
+                        id="categoryFilter"
+                        className={styles.filterContainer}
+                        value={categoryFilter}
+                        onChange={(e) => setCategoryFilter(e.target.value)}
+                    >
+                        <option value="allItems">Sort By Category</option>
+                        <option value="soups">Soups</option>
+                        <option value="mains">Mains</option>
+                        <option value="desserts">Desserts</option>
+                    </select>
+
+                    <select
+                        name="cookTimeFilter"
+                        id="cookTimeFilter"
+                        className={styles.filterContainer}
+                        value={cookTimeFilter}
+                        onChange={(e) => setCookTimeFilter(e.target.value)}
+                    >
+                        <option value="">Sort By Total Cook Time</option>
+                        <option value="under30">Under 30 min</option>
+                        <option value="30to60">30 to 60 min</option>
+                        <option value="over1hour">1 hour +</option>
+                        <option value="over2hours">2 hours</option>
+                    </select>
+
                     <div className={styles.search}>
-                            <input type="text" placeholder="Search for recipe"/>
-                            <SearchIcon className={styles.searchIcon}fontSize="medium"/>
+                        <input
+                            type="text"
+                            placeholder="Search for recipe"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+                        <SearchIcon className={styles.searchIcon} fontSize="medium" />
                     </div>
-                </section>
-                <section className={styles.cardContainer}>
-                    {recipes.map((listItems, index) => (
+                </div>
+            </section>
+
+            <section className={styles.cardContainer}>
+                {filteredRecipes.map((recipe) => (
                     <ShoppingListCard 
-                        key={index} 
-                        myShoppingList={listItems} />
-                    ))} 
-                </section>
-            </div>
-        </>
-    )
+                        key={recipe.id} // Ensure unique key is provided
+                        myShoppingList={recipe} 
+                        handleDelete={() => handleDelete(recipe.id)} 
+                    />
+                ))}
+            </section>
+        </div>
+       </>
+    );
 }
 
+export default ShoppingList;
