7041967a4aaa3c76e1ced477fdbdfb412114e0dc
1medi <144403286+1medi@users.noreply.github.com>
2024-11-18 17:21:50 -0800

    Adding Navlink and media query

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountBox from '@mui/icons-material/AccountBox';
 import Navigation from '../Navigation/index.jsx';
@@ -7,16 +7,13 @@ import '@fontsource/roboto';
 
 export default function Header() {
     return (
-        <>
         <div className={styles.header}>
-            <img src={'/LH_templogo.svg'} alt={'lutong bahay temporary logo'} width={115} height={115} className={styles.logo} />
             <Navigation />
             <div className={styles.search}>
-                <input type="text" placeholder="Search for recipe"/>
-                <SearchIcon fontSize="medium"/>
-                <AccountBox className={styles.personIcon} fontSize='large'/>
+                <input type="text" placeholder="Search for recipe" />
+                <SearchIcon fontSize="medium" />
+                <AccountBox className={styles.personIcon} fontSize="large" />
             </div>
         </div>
-        </>
-    )
+    );
 }
