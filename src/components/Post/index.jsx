e2e1f5fb5338d4a415e42ba4945291829fd64fc8
MarloweReyes <mreyes58@my.bcit.ca>
2024-12-11 14:42:09 -0800

    initial layout fixes for forum

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -38,19 +38,19 @@ export default function Editor({ setPosts }) {
         <div>
             <div className={styles.pleaseWork}>
                 <div className={styles.pfp}></div>
-                <button className={styles.postInput} type="button" onClick={handleOpenModal}>Write a post...</button>
+                <button className={styles.postInput} type="button" onClick={handleOpenModal}>Click here to write a post...</button>
             </div>
 
             {isModalOpen && (
                 <div className={styles.modal}> 
                     <div className={styles.modalContent}>
                         <div className={styles.modalHeader}>
-                            <h2>Add Content</h2>
+                            <h2>Write a Post</h2>
                             <button className={styles.button2} type="button" onClick={handleCloseModal}>X</button>
                         </div>
                         <form onSubmit={handleSubmit}>
                             <fieldset>
-                                <legend>Name of Food</legend>
+                                <legend>Title</legend>
                                 <input 
                                     type="text" 
                                     name="heading" 
@@ -59,17 +59,7 @@ export default function Editor({ setPosts }) {
                                 />
                             </fieldset>
                             <fieldset>
-                                <legend>Description</legend>
-                                <textarea 
-                                    rows="5" 
-                                    cols="50" 
-                                    name="description"
-                                    value={description}
-                                    onChange={(e) => setDescription(e.target.value)}
-                                ></textarea>
-                            </fieldset>
-                            <fieldset>
-                                <legend>Ingredients & Recipe</legend>
+                                <legend>Write a tip here</legend>
                                 <textarea 
                                     rows="5" 
                                     cols="50" 
--- src/components/Post/output.jsx
+++ src/components/Post/output.jsx
@@ -5,15 +5,14 @@ import styles from './Post.module.css';
 export default function Output({ posts, toggleLike }) {
   return (
     <div className={styles.output}>
-      {/* Map over the posts and display them */}
       {posts.map((post, index) => (
         <Post
           key={index}
           heading={post.heading}
           description={post.description}
           paragraph={post.paragraph}
-          toggleLike={toggleLike} // Pass toggleLike function
-          post={post} // Pass the post itself
+          toggleLike={toggleLike}
+          post={post} 
         />
       ))}
     </div>
